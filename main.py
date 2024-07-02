5b2e38dfc33883b563286aff93da7883f41fa1c6
Jiyeon <jxpark0044@zonemail.clpccd.edu>
2024-07-01 20:17:21 -0700

    first

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the starting letter: ')
-
+        if not start.isalpha() or not end.isalpha():
+            print('Make sure its a letter')
+            continue
+        if start > end:
+            print('The starting letter must be smaller then the ending letter')
+            continue
+        
+        break
+            
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    begin = ord(start)
+    last = ord(end)
+    for alph in (begin , last + 1):
+        alph.append(result)
 
     print(*result)
 
